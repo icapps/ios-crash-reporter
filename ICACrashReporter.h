3a74f36e91d905bf70e183375e70738a23241198
Pieterjan Vandegaer <pieterjan.vandegaer@icapps.com>
2015-02-18 10:16:44 +0100

    Initial commit

---
+++ ICACrashReporter.h
@@ -0,0 +1,29 @@
+//
+//  CRAssistent.h
+//  iCapps
+//
+//  Created by Pieterjan Vandegaer on 09/02/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+#define ICACrashReporterTransactionId NSString
+
+@interface ICACrashReporter : NSObject
+
++ (void)initAndStartSession:(NSString *)key;
++ (void)setUserIdentifier:(NSString *)userId;
++ (void)restartSession:(NSString *)key user:(NSString *)userId;
++ (void)logBreadcrumb:(NSString *)breadcrumb;
++ (void)logEvent:(NSString *)eventInfo;
++ (void)logExtraData:(NSString *)key value:(NSString *)value;
++ (void)logServiceFailure:(NSInteger)statusCode serviceUrl:(NSString *)serviceUrl httpMethod:(NSString *)httpMethod;
++ (void)logServiceFailureWithURLResponse:(NSHTTPURLResponse *)response;
++ (void)logServiceFailureWithURLResponse:(NSHTTPURLResponse *)response httpMethod:(NSString *)httpMethod;
++ (void)logException:(NSException *)exception;
++ (ICACrashReporterTransactionId *)logTransactionStart;
++ (void)logTransactionStop:(ICACrashReporterTransactionId *)transactionId;
++ (void)logTransactionCancel:(ICACrashReporterTransactionId *)transactionId reason:(NSString *)reason;
+
+@end
---
+++ ICACrashReporter.m
@@ -0,0 +1,80 @@
+//
+//  CRAssistent.m
+//  iCapps
+//
+//  Created by Pieterjan Vandegaer on 09/02/15.
+//
+//
+
+#import "ICACrashReporter.h"
+#import <SplunkMint-iOS/SplunkMint-iOS.h>
+
+@implementation ICACrashReporter
+
++ (void)initAndStartSession:(NSString *)key {
+    [[Mint sharedInstance] initAndStartSession:key];
+    [ICACrashReporter logExtraData:@"BundleId" value:[[NSBundle mainBundle] objectForInfoDictionaryKey:(NSString *)kCFBundleIdentifierKey]];
+    [ICACrashReporter logExtraData:@"BuildVersion" value:[[NSBundle mainBundle] objectForInfoDictionaryKey:(NSString *)kCFBundleVersionKey]];
+}
+
++ (void)setUserIdentifier:(NSString *)userId {
+    [[Mint sharedInstance] setUserIdentifier:userId];
+}
+
++ (void)restartSession:(NSString *)key user:(NSString *)userId {
+    [ICACrashReporter setUserIdentifier:userId];
+    [ICACrashReporter initAndStartSession:key];
+}
+
++ (void)logBreadcrumb:(NSString *)breadcrumb {
+    [[Mint sharedInstance] leaveBreadcrumb:breadcrumb];
+}
+
++ (void)logEvent:(NSString *)eventInfo {
+    [[Mint sharedInstance] logEventAsyncWithTag:eventInfo completionBlock:nil];
+}
+
++ (void)logExtraData:(NSString *)key value:(NSString *)value {
+    if (key && value) {
+        [[Mint sharedInstance] addExtraData:[[ExtraData alloc] initWithKey:key andValue:value]];
+    }
+}
+
++ (void)logServiceFailure:(NSInteger)statusCode serviceUrl:(NSString *)serviceUrl httpMethod:(NSString *)httpMethod {
+    if ([serviceUrl length] > 50) {
+        serviceUrl = [serviceUrl substringFromIndex:serviceUrl.length - 50];
+    }
+    NSString *eventInfo = [NSString stringWithFormat:@"%li: %@", (long)statusCode, serviceUrl];
+    if (httpMethod && httpMethod.length > 0) {
+        eventInfo = [NSString stringWithFormat:@"%@: %@", httpMethod, eventInfo];
+    }
+    [ICACrashReporter logEvent:eventInfo];
+}
+
++ (void)logServiceFailureWithURLResponse:(NSHTTPURLResponse *)response {
+    [ICACrashReporter logServiceFailureWithURLResponse:response httpMethod:nil];
+}
+
++ (void)logServiceFailureWithURLResponse:(NSHTTPURLResponse *)response httpMethod:(NSString *)httpMethod {
+    [ICACrashReporter logServiceFailure:response.statusCode serviceUrl:response.URL.absoluteString httpMethod:httpMethod];
+}
+
++ (void)logException:(NSException *)exception {
+    MintLogException(exception, nil);
+}
+
++ (ICACrashReporterTransactionId *)logTransactionStart {
+    ICACrashReporterTransactionId *UUID = [[NSUUID UUID] UUIDString];
+    [[Mint sharedInstance] transactionStart:UUID andResultBlock:nil];
+    return UUID;
+}
+
++ (void)logTransactionStop:(ICACrashReporterTransactionId *)transactionId {
+    [[Mint sharedInstance] transactionStop:transactionId andResultBlock:nil];
+}
+
++ (void)logTransactionCancel:(ICACrashReporterTransactionId *)transactionId reason:(NSString *)reason {
+    [[Mint sharedInstance] transactionCancel:transactionId reason:reason andResultBlock:nil];
+}
+
+@end
