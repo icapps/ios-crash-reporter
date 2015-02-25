23c409ccc300c38de6fbde55d4e4265cb57b3826
Pieterjan Vandegaer <pieterjan.vandegaer@icapps.com>
2015-02-25 12:41:55 +0100

    Cleanup demo project

--- main.m
+++ main.m
@@ -8,11 +8,24 @@
 
 #import <UIKit/UIKit.h>
 #import "ICACrashReporter.h"
+#import "ICASplunkCrashReporter.h"
+#import "ICAConsoleCrashReporter.h"
+#import "ICACrashReporterTransactionController.h"
 
 int main(int argc, char * argv[]) {
-    /*@autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
-    }*/
+    //[ICACrashReporter initAndStartWithInstance:[[ICASplunkCrashReporter alloc] initWithKey:@"bc3ef72f"]];
+    [ICACrashReporter initAndStartWithInstance:[ICAConsoleCrashReporter new]];
+    [ICACrashReporter logBreadcrumb:@"test %@ %f %@", @"x", 23.3, @"TEST"];
+
+    [ICACrashReporter logServiceFailure:404 serviceUrl:@"http://icapps.com" httpMethod:@"GET"];
+    [ICACrashReporter setUserIdentifier:@"user A"];
+    [ICACrashReporter logEvent:@"new event"];
+    [ICACrashReporter logExtraData:@"key" value:@"value"];
     
+    [ICACrashReporter logException:[NSException exceptionWithName:@"new exception" reason:@"crashed" userInfo:nil]];
     
+    ICACrashReporterTransactionController *controller = [ICACrashReporter transactionController];
+    [controller startTransaction];
+    [controller stopTransaction];
+    [controller cancelTransaction];
 }
