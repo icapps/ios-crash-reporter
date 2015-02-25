9fca25b01eb97f18fc96dde9bcda743ebc76bb70
Pieterjan Vandegaer <pieterjan.vandegaer@icapps.com>
2015-02-25 10:22:26 +0100

    Comment out splunk

--- ICACrashReporter.m
+++ ICACrashReporter.m
@@ -7,18 +7,18 @@
 //
 
 #import "ICACrashReporter.h"
-#import <SplunkMint-iOS/SplunkMint-iOS.h>
+//#import <SplunkMint-iOS/SplunkMint-iOS.h>
 
 @implementation ICACrashReporter
 
 + (void)initAndStartSession:(NSString *)key {
-    [[Mint sharedInstance] initAndStartSession:key];
+    //[[Mint sharedInstance] initAndStartSession:key];
     [ICACrashReporter logExtraData:@"BundleId" value:[[NSBundle mainBundle] objectForInfoDictionaryKey:(NSString *)kCFBundleIdentifierKey]];
     [ICACrashReporter logExtraData:@"BuildVersion" value:[[NSBundle mainBundle] objectForInfoDictionaryKey:(NSString *)kCFBundleVersionKey]];
 }
 
 + (void)setUserIdentifier:(NSString *)userId {
-    [[Mint sharedInstance] setUserIdentifier:userId];
+    //[[Mint sharedInstance] setUserIdentifier:userId];
 }
 
 + (void)restartSession:(NSString *)key user:(NSString *)userId {
@@ -37,12 +37,12 @@
 }
 
 + (void)logEvent:(NSString *)eventInfo {
-    [[Mint sharedInstance] logEventAsyncWithTag:eventInfo completionBlock:nil];
+    //[[Mint sharedInstance] logEventAsyncWithTag:eventInfo completionBlock:nil];
 }
 
 + (void)logExtraData:(NSString *)key value:(NSString *)value {
     if (key && value) {
-        [[Mint sharedInstance] addExtraData:[[ExtraData alloc] initWithKey:key andValue:value]];
+        //[[Mint sharedInstance] addExtraData:[[ExtraData alloc] initWithKey:key andValue:value]];
     }
 }
 
@@ -66,21 +66,21 @@
 }
 
 + (void)logException:(NSException *)exception {
-    MintLogException(exception, nil);
+    //MintLogException(exception, nil);
 }
 
 + (ICACrashReporterTransactionId *)logTransactionStart {
     ICACrashReporterTransactionId *UUID = [[NSUUID UUID] UUIDString];
-    [[Mint sharedInstance] transactionStart:UUID andResultBlock:nil];
+    //[[Mint sharedInstance] transactionStart:UUID andResultBlock:nil];
     return UUID;
 }
 
 + (void)logTransactionStop:(ICACrashReporterTransactionId *)transactionId {
-    [[Mint sharedInstance] transactionStop:transactionId andResultBlock:nil];
+    //[[Mint sharedInstance] transactionStop:transactionId andResultBlock:nil];
 }
 
 + (void)logTransactionCancel:(ICACrashReporterTransactionId *)transactionId reason:(NSString *)reason {
-    [[Mint sharedInstance] transactionCancel:transactionId reason:reason andResultBlock:nil];
+    //[[Mint sharedInstance] transactionCancel:transactionId reason:reason andResultBlock:nil];
 }
 
 @end
