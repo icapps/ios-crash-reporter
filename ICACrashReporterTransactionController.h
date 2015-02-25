a8d5d164cb25a17bb94a5af16348361a003c4ed8
Pieterjan Vandegaer <pieterjan.vandegaer@icapps.com>
2015-02-25 11:30:04 +0100

    Added ICACrashReporterTransactionController

---
+++ ICACrashReporterTransactionController.h
@@ -0,0 +1,13 @@
+//
+//  ICACrashReporterTransactionController.h
+//  ICACrashReporter
+//
+//  Created by Pieterjan Vandegaer on 25/02/15.
+//  Copyright (c) 2015 iCapps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ICACrashReporterTransactionController : NSObject
+
+@end
---
+++ ICACrashReporterTransactionController.m
@@ -0,0 +1,13 @@
+//
+//  ICACrashReporterTransactionController.m
+//  ICACrashReporter
+//
+//  Created by Pieterjan Vandegaer on 25/02/15.
+//  Copyright (c) 2015 iCapps. All rights reserved.
+//
+
+#import "ICACrashReporterTransactionController.h"
+
+@implementation ICACrashReporterTransactionController
+
+@end
