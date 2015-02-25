16cf9b6d77bfb969396ed7fcee49d82b27997069
Pieterjan Vandegaer <pieterjan.vandegaer@icapps.com>
2015-02-25 10:22:43 +0100

    Added test xcode project

---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ICACrashReporter
+//
+//  Created by Pieterjan Vandegaer on 23/02/15.
+//  Copyright (c) 2015 iCapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ICACrashReporter.h"
+
+int main(int argc, char * argv[]) {
+    /*@autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }*/
+    
+    
+}
