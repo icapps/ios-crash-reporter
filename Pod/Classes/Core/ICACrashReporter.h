a775c7df9548906a92782fb0fe64fbcdd112d194
Jelle Vandebeeck <jelle@fousa.be>
2015-12-22 14:02:18 +0100

    Add deprecation warnings.

--- Pod/Classes/Core/ICACrashReporter.h
+++ Pod/Classes/Core/ICACrashReporter.h
@@ -29,15 +29,15 @@
 - (ICACrashReporterTransactionController *)instantiateTransactionController;
 
 // Deprecated methods.
-+ (void)initAndStartWithInstance:(id<ICACrashReporterProvider>)instance;
-+ (void)setUserIdentifier:(NSString *)userId;
-+ (void)logBreadcrumb:(NSString *)message, ...;
-+ (void)logEvent:(NSString *)eventInfo;
-+ (void)logExtraData:(NSString *)key value:(NSString *)value;
-+ (void)logServiceFailure:(NSInteger)statusCode serviceUrl:(NSString *)serviceUrl httpMethod:(NSString *)httpMethod;
-+ (void)logServiceFailureWithURLResponse:(NSHTTPURLResponse *)response;
-+ (void)logServiceFailureWithURLResponse:(NSHTTPURLResponse *)response httpMethod:(NSString *)httpMethod;
-+ (void)logException:(NSException *)exception;
-+ (ICACrashReporterTransactionController *)transactionController;
++ (void)initAndStartWithInstance:(id<ICACrashReporterProvider>)instance DEPRECATED_MSG_ATTRIBUTE("Use the instance methods 'setProviders:' on the sharedInstance instead.");
++ (void)setUserIdentifier:(NSString *)userId DEPRECATED_MSG_ATTRIBUTE("Use the instance methods 'setUserIdentifier:' on the sharedInstance instead.");
++ (void)logBreadcrumb:(NSString *)message, ... DEPRECATED_MSG_ATTRIBUTE("Use the instance methods 'logBreadcrumb:' on the sharedInstance instead.");;
++ (void)logEvent:(NSString *)eventInfo DEPRECATED_MSG_ATTRIBUTE("Use the instance methods 'logEvent:' on the sharedInstance instead.");;
++ (void)logExtraData:(NSString *)key value:(NSString *)value DEPRECATED_MSG_ATTRIBUTE("Use the instance methods 'logKey:value:' on the sharedInstance instead.");;
++ (void)logServiceFailure:(NSInteger)statusCode serviceUrl:(NSString *)serviceUrl httpMethod:(NSString *)httpMethod DEPRECATED_MSG_ATTRIBUTE("Use the instance methods 'logServiceFailureWithStatusCode:serviceURL:HTTPMethod:' on the sharedInstance instead.");;
++ (void)logServiceFailureWithURLResponse:(NSHTTPURLResponse *)response DEPRECATED_MSG_ATTRIBUTE("Use the instance methods 'logServiceFailureWithResponse:' on the sharedInstance instead.");;
++ (void)logServiceFailureWithURLResponse:(NSHTTPURLResponse *)response httpMethod:(NSString *)httpMethod DEPRECATED_MSG_ATTRIBUTE("Use the instance methods 'logServiceFailureWithResponse:HTTPMethod:' on the sharedInstance instead.");;
++ (void)logException:(NSException *)exception DEPRECATED_MSG_ATTRIBUTE("Use the instance methods 'logException:' on the sharedInstance instead.");;
++ (ICACrashReporterTransactionController *)transactionController DEPRECATED_MSG_ATTRIBUTE("Use the instance methods 'instantiateTransactionController' on the sharedInstance instead.");;
 
 @end
