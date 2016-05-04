7a9d0698979f08c6a940f7bad507306daa7b0f45
Hannes Van den Berghe <hannes.van.den.berghe@live.be>
2016-05-04 08:40:22 +0200

    Added CrittercismObject in Example project

---
+++ Example/Objective-C/ICACrashReporter/CrittercismObject.m
@@ -0,0 +1,13 @@
+//
+//  CrittercismObject.m
+//  ICACrashReporter
+//
+//  Created by Hannes Van den Berghe on 04/05/16.
+//  Copyright © 2016 Jelle Vandebeeck. All rights reserved.
+//
+
+#import "CrittercismObject.h"
+
+@implementation CrittercismObject
+
+@end
