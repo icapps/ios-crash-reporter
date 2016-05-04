a152faf64d8c3f52adb8aa330f756224fc2dd8f6
Hannes Van den Berghe <hannes.van.den.berghe@live.be>
2016-05-04 09:02:05 +0200

    Added crittercismObject

---
+++ Example/Swift/ICACrashReporter/CrittercismObject.swift
@@ -0,0 +1,48 @@
+//
+//  CrittercismObject.swift
+//  ICACrashReporter Example
+//
+//  Created by Hannes Van den Berghe on 04/05/16.
+//  Copyright © 2016 Jelle Vandebeeck. All rights reserved.
+//
+
+import Foundation
+import ICACrashReporter
+
+class CrittercismObject {
+    
+    // MARK: - Init
+    
+    init() {
+        setupCrashReporter()
+    }
+    
+    // MARK: - Setup
+    
+    private func setupCrashReporter() {
+        // Setup the reporters.
+        let reporters = [
+            ICAConsoleCrashReporter(),
+            ICACrittercismCrashReporter()
+        ]
+        let provider = ICAMultiCrashReporter(reporters: reporters)
+        ICACrashReporter.sharedInstance().provider = provider
+        
+        // Run some reporting tools.
+        ICACrashReporter.sharedInstance().logServiceFailureWithStatusCode(404, serviceURL: "http://icapps.com", HTTPMethod: "GET")
+        ICACrashReporter.sharedInstance().logBreadcrumb("KAWA BUNGA")
+        ICACrashReporter.sharedInstance().userIdentifier = "123"
+        ICACrashReporter.sharedInstance().logEvent("BOOM!")
+        ICACrashReporter.sharedInstance().logKey("a", value: "b")
+        
+        // Log an exception.
+        let exception = NSException(name: "BOOM", reason: "Inconsistent design", userInfo: nil)
+        ICACrashReporter.sharedInstance().logException(exception)
+        
+        // Use the transaction controller.
+        let controller = ICACrashReporter.sharedInstance().instantiateTransactionController()
+        controller.start()
+        controller.stop()
+        controller.cancel()
+    }
+}
